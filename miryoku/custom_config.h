52900d2c08013a205639e7d5be44542e22bd756c
Artur Iskhakov <sizieks@users.noreply.github.com>
2025-04-08 22:24:16 +0500

    fix: use custom combos

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -16,8 +16,8 @@ K30  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  K39 \
 
 #define MIRYOKU_LAYER_BASE \
 &kp Q,             &kp W,         &kp F,              &kp P,              &kp B,              &kp J,            &kp L,             &kp U,            &kp Y,           &kp SQT,               \
-U_MT(LGUI, A),     U_MT(LALT, R), U_MT(LCTRL, S),     U_MT(LSHFT, T),     &kp G,              &kp K,            U_MT(LSHFT, N),    U_MT(LCTRL, E),   U_MT(LALT, I),   U_MT(LGUI, O),         \
-U_LT(U_BUTTON, Z), U_MT(RALT, X), &kp C,              &kp D,              &kp V,              &kp M,            &kp H,             &kp COMMA,        U_MT(RALT, DOT), U_LT(U_BUTTON, SLASH), \
+U_MT(LGUI, A),     U_MT(LALT, R), U_MT(LCTRL, S),     U_MT(LSHFT, T),     &kp G,              &kp M,            U_MT(LSHFT, N),    U_MT(LCTRL, E),   U_MT(LALT, I),   U_MT(LGUI, O),         \
+U_LT(U_BUTTON, Z), U_MT(RALT, X), &kp C,              &kp D,              &kp V,              &kp K,            &kp H,             &kp COMMA,        U_MT(RALT, DOT), U_LT(U_BUTTON, SLASH), \
 &kp CAPS,          U_NP,          U_LT(U_MEDIA, ESC), U_LT(U_NAV, SPACE), U_LT(U_MOUSE, TAB), U_LT(U_SYM, RET), U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL), U_NP,            &kp SEMI
 
 #define MIRYOKU_LAYER_EXTRA \
@@ -35,11 +35,3 @@ U_LT(U_BUTTON, Z), U_MT(RALT, X), &kp C,              &kp V,              &kp B,
 #define MIRYOKU_KLUDGE_MOUSEKEYSPR
 
 #define MIRYOKU_CLIPBOARD_MAC
-
-#define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 1 2
-#define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTINNERINDEX 2 3
-#define MIRYOKU_KLUDGE_TOPROWCOMBOS
-
-#define MIRYOKU_KLUDGE_BOTTOMROWCOMBOS_LEFTPINKIE 22 23
-#define MIRYOKU_KLUDGE_BOTTOMROWCOMBOS_LEFTINNERINDEX 23 24
-#define MIRYOKU_KLUDGE_BOTTOMROWCOMBOS
